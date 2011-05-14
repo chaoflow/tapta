930f90036c281c3f6a354603ecace3bdab52c7fa
Florian Friesdorf <flo@chaoflow.net>
2011-05-14 14:01:20 +0200

    cosmetics

--- src/cone/activities/browser/static/activities/localstorage.js
+++ src/cone/activities/browser/static/activities/localstorage.js
@@ -137,10 +137,18 @@ define([
         }
 
         switch (method) {
-        case "read":    resp = model.id ? store.find(model) : store.findAll(); break;
-        case "create":  resp = store.create(model);                            break;
-        case "update":  resp = store.update(model);                            break;
-        case "delete":  resp = store.destroy(model);                           break;
+        case "create":
+            resp = store.create(model);
+            break;
+        case "read":
+            resp = model.id ? store.find(model) : store.findAll();
+            break;
+        case "update":
+            resp = store.update(model);
+            break;
+        case "delete":
+            resp = store.destroy(model);
+            break;
         }
         
         if (resp) {
