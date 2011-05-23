45b9328dc27fe7a5bffa7bcf825d142b38443489
Florian Friesdorf <flo@chaoflow.net>
2011-05-23 12:17:06 +0200

    base.View: assign parent and name + debug log render

--- tapta/activities/base.js
+++ tapta/activities/base.js
@@ -3,11 +3,19 @@ define([
     'cdn/underscore',
     'cdn/backbone.js'
 ], function(require) {
+    var DEBUG_RENDER = true;
     var View = Backbone.View.extend({
         defchild: function(View, props) {
-            props.parent = props.parent || this;
             var child = new View(props);
+            child.name = props.name;
+            child.parent = props.parent || this;
             child.bind("all", _.bind(this.eventForwarder, this));
+            var realrender = child.render;
+            child.render = function() {
+                console.log("DEBUG:RENDER: " + (child.name || "unnamed view"));
+                debugger;
+                realrender.apply(this, arguments);
+            };
             return child;
         },
         eventForwarder: function() {
