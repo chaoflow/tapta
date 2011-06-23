4c2da0090e264a2626cc471a8cccef374a440389
Florian Friesdorf <flo@chaoflow.net>
2011-06-23 07:16:09 +0200

    included underscore tests

--- tapta/activities/tests.js
+++ tapta/activities/tests.js
@@ -1,6 +1,17 @@
 // Register all test modules here
 define([
     'require',
+    'jquery',
+    'vendor/qunit.js',
+    'vendor/jslitmus.js',
+    'vendor/underscore.js',
+    'vendor/underscore/test/collections.js',
+    'vendor/underscore/test/arrays.js',
+    'vendor/underscore/test/functions.js',
+    'vendor/underscore/test/objects.js',
+    'vendor/underscore/test/utility.js',
+    'vendor/underscore/test/chaining.js',
+    'vendor/underscore/test/speed.js',
     './test_base',
     './test_graphutils',
     './test_graph',
