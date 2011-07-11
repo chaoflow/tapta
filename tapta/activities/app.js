7cb22ba867e55420e05facd83027346788b9a4ad
Florian Friesdorf <flo@chaoflow.net>
2011-07-11 22:32:55 +0200

    destroy all defined as top pane

--- tapta/activities/app.js
+++ tapta/activities/app.js
@@ -1,33 +1,37 @@
 define([
     'require',
+    'jquery',
+    './base',
     './panes',
     './view'
 ], function(require) {
-    var model = require('./model'),
+    var DEBUG = require('./debug'),
+        base = require('./base'),
         panes = require('./panes'),
         Layers = require('./view').Layers;
 
     var AppView = panes.PaneManager.extend({
         panescfg: [
+            {name: "top", content: [
+                {
+                    ViewProto: base.View.extend({
+                        className: "destroyall",
+                        render: function() { $(this.el).text("DESTROY ALL"); return this; }
+                    }),
+                    props: {name: "destroyall"}
+                }
+            ]},
             {name: "center", content: [
-                [Layers, function() {
-                    // will be evaluated in the context of the new AppView
-                    return {name: "layers", model: this.model};
-                }]
+                 {
+                     ViewProto: Layers,
+                     propscallback: function() {
+                         // will be evaluated in the context of the new AppView
+                         return {name: "layers", model: this.model};
+                     }
+                 }
             ]}
         ]
         // initialize: function() {
-        //     _.bindAll(this, 'render');
-        //     // the layers view piggy-backs on our model as
-        //     // this.model.layers is not a collection but just a plain
-        //     // list for now.
-        //     // this.layers = this.defchild(Layers, {
-        //     //     // this element already exists in the App template
-        //     //     // XXX: the id should be unique
-        //     //     el: this.$('#layers'),
-        //     //     model: this.model,
-        //     //     name: "layers"
-        //     // });
         //     // var app = this;
         //     // this.$("#destroyall").click(function() {
         //     //     localStorage.clear();
