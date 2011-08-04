fdb9e822d385904f31a72f33d7adb0aa33cdafdf
Florian Friesdorf <flo@chaoflow.net>
2011-08-04 16:48:23 +0200

    cleanup

--- tapta/activities/controller.js
+++ tapta/activities/controller.js
@@ -138,7 +138,6 @@ define([
                 "controller:" +this.abspath() + ": "
                     + [event, info.view.name].join(", ")
             );
-
             switch (event) {
             case "click":
                 if ((info.view.model.type === "action") &&
@@ -174,30 +173,6 @@ define([
                 this.layerview.editmode = info;
                 break;
             }
-
-            // supported operations
-            //
-            // add node: (click/drag)
-            // sources:
-            // - new node icon
-            // - lib element
-            // targets:
-            // - edge
-            // - ctrl area right of mimo
-            //
-            // move node (editmode: select):
-            // - drag final, drop on ctrl area left of MIMO
-            // - drag action, drop on edge (cut and paste)
-
-                // current arc needs to be rerendered
-                //
-                // view for new vertex needs to be created and rendered
-                // --> eg. via add
-                //
-                // arc between new vertex and old target needs to be created and rendered
-                // target has new geometry and needs to be moved
-
-
             DEBUG && console.groupEnd();
         }
     });
